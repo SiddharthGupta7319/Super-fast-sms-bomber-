ef844f24e2dbe4bc6c4ce2e87d5fbdda718de9da
Siddharthgupta731969 <60921779+Siddharthgupta731969@users.noreply.github.com>
2020-05-26 09:00:57 +0530

    Add files via upload

---
+++ smsbombersms.sh
@@ -0,0 +1,113 @@
+# speedxgbgg
+#!/bin/bash
+clear
+echo -e "\e[4;31m siddharth's Productions !!! \e[0m"
+echo -e "\e[4;31m if you want more hacks contacts on what's app no. +1(619) 353-3354  !!! \e[0m"
+echo -e "\e[1;34m ⓾ Hacker's Siddharth Presents \e[0m"
+echo -e "\e[4;31m presents !!! \e[0m"
+echo -e "\e[1;32m TBomb \e[0m"
+echo "Press Enter To Continue"
+read a1
+if [[ -s update.hackboom ]];then
+echo "All Requirements Found...."
+else
+echo 'Installing Requirements....'
+echo .
+echo .
+apt install figlet toilet python curl -y
+apt install python3-pip
+pip install -r requirements.txt
+echo This Script Was Made By Siddharth gupta >update. Siddharthspeedx
+echo Requirements Installed....
+echo Press Enter To Continue...
+read upd
+fi
+while :
+do
+rm *.xxx >/dev/null 2>&1
+clear
+echo -e "\e[1;31m"
+figlet sms and call bomber
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border SpeedX
+echo -e "\e[4;34m This Bomber Was Created By Siddharth gupta \e[0m"
+echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
+echo -e "\e[1;32m           Mail: a12345678ggj@gmail.com \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/mantesh Mahajan \e[0m"
+echo " "
+echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
+echo " "
+echo "Press 1 To  Start SMS Bomber "
+echo "Press 2 To  Start Call Bomber "
+echo "Press 3 To  Update (Works On Linux And Linux Emulators) "
+echo "Press 4 To  View Features "
+echo "Press 5 To  Exit "
+read ch
+if [ $ch -eq 1 ];then
+clear
+echo -e "\e[1;32m"
+rm *.xxx >/dev/null 2>&1
+python3 smsbombersms.py
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 2 ];then
+clear
+echo -e "\e[1;32m"
+echo 'Call Bomb By SpeedX'> call.xxx
+python3 smsbombersms.py call
+rm *.xxx >/dev/null 2>&1
+exit 0
+elif [ $ch -eq 3 ];then
+clear
+apt install git -y
+echo -e "\e[1;34m Downloading Latest Files..."
+git clone https://github.com/Siddharthgupta731969/speedxgbgg/edit/master/README.md
+if [[ -s TBomb/TBomb.sh ]];then
+cd TBomb
+cp -r -f * .. > temp
+cd ..
+rm -rf  TBomb >> temp
+rm update.speedx >> temp
+rm temp
+chmod +x TBomb.sh
+fi
+echo -e "\e[1;32m TBomb Will Restart Now..."
+echo -e "\e[1;32m All The Required Packages Will Be Installed..."
+echo -e "\e[1;34m Press Enter To Proceed To Restart..."
+read a6
+./smsbombersms.sh
+exit
+elif [ $ch -eq 4 ];then
+clear
+echo -e "\e[1;33m"
+figlet TBomb
+echo -e "\e[1;34mCreated By \e[1;34m"
+toilet -f mono12 -F border SpeedX
+echo  " "
+echo -e "\e[1;32m                   Features\e[1;34m"
+echo "  [+] Unlimited And Super-Fast Bombing"
+echo "  [+] International Bombing"
+echo "  [+] Call Bombing "
+echo "  [+] Protection List"
+echo "  [+] Automated Future Updates"
+echo "  [+] Easy To Use And Embed in Code"
+echo -e "\e[1;32m                   Contributors\e[1;33m"
+echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
+echo "         [-] Mail At: a12345678ggj@gmail.com"
+echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
+echo "         [-] Ping At: http://wa.me/917600140353"
+echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
+echo "         [-] Ping At: https://t.me/Rieltar"
+echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
+echo "         [-] Mail At: 0n1cOn3@gmx.ch"
+read a3
+clear
+elif [ $ch -eq 5 ];then
+clear
+figlet TBomb
+exit 0
+else
+read a3
+clear
+fi
+done
