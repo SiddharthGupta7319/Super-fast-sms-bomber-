db44413f506ac32f268bfefd3d433330a38739c4
SiddharthGupta7319 <60921779+SiddharthGupta7319@users.noreply.github.com>
2020-08-30 13:34:56 +0530

    smsbombersms.sh

--- smsbombersms.sh
+++ smsbombersms.sh
@@ -29,11 +29,11 @@ clear
 echo -e "\e[1;31m"
 figlet sms and call bomber
 echo -e "\e[1;34m Created By \e[1;32m"
-toilet -f mono12 -F border SpeedX
+toilet -f mono12 -F border Deshi Dêvëløpēr's group
 echo -e "\e[4;34m This Bomber Was Created By Siddharth gupta \e[0m"
 echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
 echo -e "\e[1;32m           Mail: a12345678ggj@gmail.com \e[0m"
-echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/c/mantesh Mahajan \e[0m"
+echo -e "\e[4;32m   YouTube Page: https://www.youtube.com/channel/UC3wEn47dcuf-KM3Ydqd8SUQ \e[0m"
 echo " "
 echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
 echo " "
@@ -53,7 +53,7 @@ exit 0
 elif [ $ch -eq 2 ];then
 clear
 echo -e "\e[1;32m"
-echo 'Call Bomb By SpeedX'> call.xxx
+echo 'Call Bomb By Deshi Dêvêløper'> call.xxx
 python3 smsbombersms.py call
 rm *.xxx >/dev/null 2>&1
 exit 0
@@ -61,15 +61,15 @@ elif [ $ch -eq 3 ];then
 clear
 apt install git -y
 echo -e "\e[1;34m Downloading Latest Files..."
-git clone https://github.com/Siddharthgupta731969/speedxgbgg/edit/master/README.md
-if [[ -s TBomb/TBomb.sh ]];then
-cd TBomb
+git clone https://github.com/SiddharthGupta7319/Super-fast-sms-bomber-
+if [[ -s smsBomb/smsBomb.sh ]];then
+cd Super-fast-sms-bomber-
 cp -r -f * .. > temp
 cd ..
-rm -rf  TBomb >> temp
+rm -rf  Super-fast-sms-bomber- >> temp
 rm update.speedx >> temp
 rm temp
-chmod +x TBomb.sh
+chmod +x 
 fi
 echo -e "\e[1;32m TBomb Will Restart Now..."
 echo -e "\e[1;32m All The Required Packages Will Be Installed..."
@@ -94,10 +94,11 @@ echo "  [+] Easy To Use And Embed in Code"
 echo -e "\e[1;32m                   Contributors\e[1;33m"
 echo -e "\e[1;33m      [*]  SpeedX   \e[1;31m"
 echo "         [-] Mail At: a12345678ggj@gmail.com"
-echo -e "\e[1;33m      [*]  The Black Hacker Roxstar   \e[1;31m"
+echo -e "\e[1;33m      [*]  The Deshi Dêvêløper   \e[1;31m"
 echo "         [-] Ping At: http://wa.me/917600140353"
 echo -e "\e[1;33m      [*]  Rieltar   \e[1;31m"
-echo "         [-] Ping At: https://t.me/Rieltar"
+echo "         [-] Ping At: Deshi Dêvêløper
+https://t.me/deshidevloper"
 echo -e "\e[1;33m      [*]  0n1cOn3 (Stefan)   \e[1;31m"
 echo "         [-] Mail At: 0n1cOn3@gmx.ch"
 read a3
